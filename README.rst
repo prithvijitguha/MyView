de65eaba8664ca6d7c7b575a3514d85484f3d7af
prithvijitguha <prithvijit_guha2@hotmail.com>
2022-01-02 15:52:04 +0530

    github deploy

---
+++ README.rst
@@ -0,0 +1,4 @@
+MyView
+=======
+
+**MyView** is a website to upload and play your favorite user generated videos
---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
