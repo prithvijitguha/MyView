549a36082ff3a91af316a22c8364fff4fb22dca9
prithvijitguha <prithvijit_guha2@hotmail.com>
2022-01-04 17:09:40 +0530

    fixed pylint issue

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 MyView all views saved here
 """
 
+# pylint: disable=import-error
+# pylint: disable=invalid-name
+
+
 from fastapi import FastAPI, Request, Depends, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -26,6 +30,9 @@ models.Base.metadata.create_all(bind=engine)
 
 
 def get_db():
+    """
+    Connects to database
+    """
     db = SessionLocal()
     try:
         yield db
@@ -35,6 +42,20 @@ def get_db():
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    """
+    Function to create a user
+    First check if user exists.
+    Args:
+        - user: schemas.UserCreate
+        - db: Session
+
+    Returns:
+        - db_user
+
+    Raises:
+        - HTTPException if email already registered
+
+    """
     db_user = crud.get_user_by_email(db, email=user.email)
     # check if email already exists
     if db_user:
@@ -44,6 +65,22 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
+    """
+    Function to read user table.
+    Check if user exists.
+    Raise error if not found
+
+    Args:
+        - user_id: int
+        - db: Session
+
+    Returns:
+        - user
+
+    Raises:
+        - HTTPException if user not found
+
+    """
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
