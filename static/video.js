4feb6644d02f6bc6def3d27ef1cea43f9dff52d8
prithvijitguha <prithvijit_guha2@hotmail.com>
2022-01-25 21:34:28 +0530

    added video.js and like logic to be implemented

---
+++ static/video.js
@@ -0,0 +1,26 @@
+function like_unlike() {
+    like_button =  document.getElementById("likeButton")
+    //if like value is true
+
+    //change value to false
+
+    //if value is false
+
+    //change value to true
+
+    //fetch request to update value in video
+}
+
+
+function dislike_undislike() {
+    dislike_button =  document.getElementById("dislikeButton")
+    //if like value is true
+
+    //change value to false
+
+    //if value is false
+
+    //change value to true
+
+    //fetch request to update value in video
+}
