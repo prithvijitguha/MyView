bb4e3d185933d04feee7ac85d89fb71720983182
prithvijitguha <prithvijit_guha2@hotmail.com>
2022-02-05 16:23:59 +0530

    ENH: delete comment functionality

--- static/video.js
+++ static/video.js
@@ -86,3 +86,14 @@ function submitComment() {
     console.error('Error:', error);
     });
 }
+
+
+function deleteComment(htmlElementId) {
+    console.log(htmlElementId)
+    comment_id = htmlElementId.slice(16)
+    console.log(comment_id)
+    //fetch request to delete comment
+
+    //get the parent element and delete it
+
+}
