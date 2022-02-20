9eacc75be32191aebfb03b9df2774094d310bec3
Prithvijit <71138854+prithvijitguha@users.noreply.github.com>
2022-02-20 20:30:33 +0530

    :wrench: configuration sphinx added automock_import (#192)
    * :wrench: sphinx config automock imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,8 @@ release = "0.1"
 # ones.
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
+autodoc_mock_imports = ["fastapi", "SQLAlchemy", "python-jose", "tests"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
